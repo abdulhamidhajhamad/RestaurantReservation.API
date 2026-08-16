320f4e843818e0f810f2a4cebff90b071ca1d199
abdulhamidhajhamad <abdulhamidhajhamad@gmail.com>
2026-08-17 00:18:26 +0300

    setup everythings and connect with db

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<RestaurantReservationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "API Ready!");
+
+app.Run();
